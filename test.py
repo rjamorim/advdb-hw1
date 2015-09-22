305444978e4163d764c572f81cc4fe26894fd0ef
pff2108 <pff2108@columbia.edu>
2015-09-22 03:20:56 -0400

    Distance function added

--- test.py
+++ test.py
@@ -1,10 +1,69 @@
 import urllib2
 import base64
 import json
+from sys import maxsize
 from collections import defaultdict
 
+
+def get_distance(fragments, relevant, test_list, query_list):
+    # get_distance
+
+    location_att = defaultdict(float)
+    word_count = defaultdict(int)
+
+    for i in relevant:
+        # Read the correspondent relevant fragment
+        text = fragments[i - 1]
+        print str(i) + ':'
+        print text
+        word_location = defaultdict(set)
+        # Determine position of the words in the query
+        for word in query_list:
+            count = text.count(word)
+            list_location = set()
+            location = 0
+            while count > 0:
+                new_location = text[location:].index(word)
+                location += new_location
+                list_location.add(location)
+                location += 1
+                count -= 1
+            word_location[word] = list_location
+            print word, word_location[word]
+        # Determine position of the words that could be added to the query
+        for word in test_list:
+            count0 = count = float(text.count(word))
+            if count0 != 0:
+                word_count[word] += count
+                list_location = set()
+                location = 0
+                while count > 0:
+                    new_location = text[location:].index(word)
+                    location += new_location
+                    list_location.add(location)
+                    # Calculate distance and relative position to each word in the query
+                    for word2 in query_list:
+                        dist = maxsize
+                        rel_pos = 0
+                        for location2 in word_location[word2]:
+                            if abs(location - location2) < dist:
+                                dist = abs(location - location2)
+                                rel_pos = (location - location2) / abs(location - location2)
+                        location_att[(word, word2, 'dist')] += dist
+                        location_att[(word, word2, 'rel_pos')] += rel_pos
+                    location += 1
+                    count -= 1
+                word_location[word] = list_location
+                print word, word_location[word]
+
+    for entry in location_att.keys():
+        location_att[entry] /= word_count[entry[0]]
+
+    return location_att
+
+
 def relevance():
-    print "Please input the relevant results to your query in the line below, with space saparated numbers:"
+    print "Please input the relevant results to your query in the line below, with space separated numbers:"
     relevantstr = raw_input('> ')
     relevantstr = relevantstr.lstrip()
     print relevantstr
@@ -40,9 +99,8 @@ def relevance():
 def runQuery(query):
     query_list = query.split(' ')
     query_url = urllib2.quote("'" + query + "'")
-    bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=' + query_url + '&$top=10&$format=json' #&format=Atom'
-    #bingUrl = 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27gates%27&Options=%27EnableHighlighting%27&WebSearchOptions=%27DisableQueryAlterations%27&Market=%27ar-XA%27&Adult=%27Off%27&Latitude=0&Longitude=10&WebFileType=%27HTML%27'
-    #Provide your account key here
+    bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=' + query_url + '&$top=10&$format=json'
+    # Provide your account key here
     accountKey = 'hTvGEgXTQ8lDLYr8nnHocn7n9GSwF5antgnogEhNDTc'
 
     accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
@@ -50,8 +108,8 @@ def runQuery(query):
     req = urllib2.Request(bingUrl, headers = headers)
     response = urllib2.urlopen(req)
     content = response.read()
-    #content contains the xml/json response from Bing.
-    #print content
+    # content contains the xml/json response from Bing.
+    # print content
     tree = json.loads(content)
     results = tree['d']['results']
 
@@ -63,13 +121,13 @@ def runQuery(query):
         print str(i) + ': ' + entry['Title']
         print '\t' + entry['Url']
         print '\t' + entry['Description'] + '\n'
-        text = (entry['Title'] + ' ' + entry['Description']).lower()
+        text = (entry['Title'] + ' - ' + entry['Description']).lower()
         # Here we ignore punctuation marks
-        for ch in [", ", ". ", "... ", "...", " - ", "! ", "? ", ") ", " (", "&", "/"]:
+        for ch in [", ", ". ", "... ", " ...", " - ", "! ", "? ", ") ", " (", " & ", "/"]:
             if ch in text:
                 text = text.replace(ch, " _IGNORE_ ")
         # Here we ignore common, irrelevant words
-        #for ch in ["and", "or", "of", "is", "are", "from", "the", "but", "i", "a", "an"]:
+        # for ch in ["and", "or", "of", "is", "are", "from", "the", "but", "i", "a", "an"]:
         #    if ch in text:
         #        text = text.replace(ch, " _IGNORE_ ")
         text_split = text.split(' ')
@@ -85,11 +143,15 @@ def runQuery(query):
     for word in word_count.keys():
         pos = len(word_count[word].intersection(relevant))
         neg = len(word_count[word].difference(relevant))
-        result = (float(pos) / n_relevant) * ((10. - n_relevant - neg) / (10. - n_relevant))
-        test.append((word, pos, neg, result))
+        score = (float(pos) / n_relevant) * ((10. - n_relevant - neg) / (10. - n_relevant))
+        test.append((word, pos, neg, score))
 
-    sorted_test = sorted(test, key=lambda word: word[3], reverse=True)[:10]
+    sorted_test = sorted(test, key=lambda word_att: word_att[3], reverse=True)[:10]
     print sorted_test
+
+    location_att = get_distance(fragments, relevant, sorted_test[0], query_list)
+    print location_att[(sorted_test[0][0], query_list[0], 'dist')]
+    print location_att[(sorted_test[0][0], query_list[0], 'rel_pos')]
 
     return fragments, sorted_test
 
